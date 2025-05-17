8f743483db446be793eded284b544d6ae4208f31
Karateev <karateev.92@mail.ru>
2025-05-17 18:52:05 +0400

    Добавлен namespace и проброс MEDIA в urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from .apps import CatalogConfig  # добавляем имя приложения
+
+app_name = CatalogConfig.name  # устанавливаем пространство имён
 
 urlpatterns = [
     path('', views.home, name='home'),
