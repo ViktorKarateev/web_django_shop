339679ab1bab3a4bd099a0008fb24e5198aa98e3
Karateev <karateev.92@mail.ru>
2025-05-18 18:05:43 +0400

    Обновлён шаблон home: вывод товаров, описание до 100 символов

--- catalog/views.py
+++ catalog/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import get_object_or_404
 from .models import Product
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    products = Product.objects.all()
+    return render(request, 'catalog/home.html', {'products': products})
 
 def contacts(request):
     success = False
