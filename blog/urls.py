57e02f0429bf3f5ddbda3632cbd10abfbc9e74be
karat <karateev.92@gmail.com>
2025-05-25 23:23:43 +0400

    реализован CRUD для блоговых записей через CBV

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from . import views
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView
+)
 
 app_name = 'blog'
 
 urlpatterns = [
-    # пока пусто — заполним позже
+    path('', BlogListView.as_view(), name='blog_list'),
+    path('<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('create/', BlogCreateView.as_view(), name='blog_create'),
+    path('<int:pk>/update/', BlogUpdateView.as_view(), name='blog_update'),
+    path('<int:pk>/delete/', BlogDeleteView.as_view(), name='blog_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import BlogPost
 
-# Create your views here.
+
+class BlogListView(ListView):
+    model = BlogPost
+    template_name = 'blog/blog_list.html'
+    context_object_name = 'posts'
+
+
+class BlogDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blog/blog_detail.html'
+    context_object_name = 'post'
+
+
+class BlogCreateView(CreateView):
+    model = BlogPost
+    fields = ['title', 'content', 'preview', 'is_published']
+    template_name = 'blog/blog_form.html'
+    success_url = reverse_lazy('blog:blog_list')
+
+
+class BlogUpdateView(UpdateView):
+    model = BlogPost
+    fields = ['title', 'content', 'preview', 'is_published']
+    template_name = 'blog/blog_form.html'
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()  # временно — до доработки
+
+
+class BlogDeleteView(DeleteView):
+    model = BlogPost
+    template_name = 'blog/blog_confirm_delete.html'
+    success_url = reverse_lazy('blog:blog_list')
