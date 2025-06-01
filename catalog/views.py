898ce752fdb208e3d545ddbd04bcd8c706a80149
karat <karateev.92@gmail.com>
2025-06-01 12:57:38 +0400

    Добавлено представление для редактирования продукта

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,13 @@
 from django.views.generic import ListView, DetailView, TemplateView
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
-
+from .forms import ProductForm
 
 class ProductListView(ListView):
     model = Product
     template_name = 'catalog/home.html'
     context_object_name = 'page_obj'
     paginate_by = 6
-
 
 class ProductDetailView(DetailView):
     model = Product
@@ -17,3 +17,14 @@ class ProductDetailView(DetailView):
 
 class ContactsView(TemplateView):
     template_name = 'catalog/contacts.html'
+
+def update_product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('catalog:product_detail', pk=product.pk)
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'catalog/product_update.html', {'form': form, 'product': product})
