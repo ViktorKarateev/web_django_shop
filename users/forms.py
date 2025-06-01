62065faf60c8feb480b0740a5be0bbfcef76e815
karat <karateev.92@gmail.com>
2025-06-01 21:54:37 +0400

    Реализована аутентификация пользователей с редактированием профиля

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
+
 class UserRegisterForm(UserCreationForm):
     class Meta:
         model = CustomUser
@@ -13,6 +14,25 @@ class UserRegisterForm(UserCreationForm):
             'country': forms.TextInput(attrs={'class': 'form-control'}),
             'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
             'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.setdefault('class', 'form-control')
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'email', 'avatar', 'phone', 'country']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'avatar': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'country': forms.TextInput(attrs={'class': 'form-control'}),
         }
 
     def __init__(self, *args, **kwargs):
--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
-from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 from django.views.generic.edit import CreateView
+from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from django.conf import settings
+from django.urls import reverse_lazy
 
 from .models import CustomUser
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, ProfileForm
+
 
 class RegisterView(CreateView):
     model = CustomUser
@@ -14,7 +17,6 @@ class RegisterView(CreateView):
 
     def form_valid(self, form):
         response = super().form_valid(form)
-        # Отправка приветственного письма
         send_mail(
             subject='Добро пожаловать!',
             message='Спасибо за регистрацию в нашем магазине!',
@@ -23,3 +25,16 @@ class RegisterView(CreateView):
             fail_silently=True,
         )
         return response
+
+
+@login_required
+def update_profile_view(request):
+    user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('catalog:home')
+    else:
+        form = ProfileForm(instance=user)
+    return render(request, 'users/profile_edit.html', {'form': form})
