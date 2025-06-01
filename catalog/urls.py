bc1d2669e22da197f45ebf56b50799e1887a168e
karat <karateev.92@gmail.com>
2025-06-01 13:13:46 +0400

    Добавлен маршрут для редактирования продукта

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView, ContactsView
+from .views import ProductListView, ProductDetailView, ContactsView, update_product_view
 
 app_name = 'catalog'
 
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('product/<int:pk>/update/', update_product_view, name='product_update'),
 ]
-
