bc1bfed8a46f1db139eb8a0eaf4cc74490d28caf
karat <karateev.92@gmail.com>
2025-06-01 21:33:27 +0400

    Добавлена регистрация пользователя с формой и отправкой письма

---
+++ users/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'password1', 'password2', 'avatar', 'phone', 'country']
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'avatar': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'country': forms.TextInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.setdefault('class', 'form-control')
--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.core.mail import send_mail
+from django.conf import settings
 
-# Create your views here.
+from .models import CustomUser
+from .forms import UserRegisterForm
+
+class RegisterView(CreateView):
+    model = CustomUser
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('catalog:home')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        # Отправка приветственного письма
+        send_mail(
+            subject='Добро пожаловать!',
+            message='Спасибо за регистрацию в нашем магазине!',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[form.cleaned_data['email']],
+            fail_silently=True,
+        )
+        return response
