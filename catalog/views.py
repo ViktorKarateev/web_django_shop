181a5145c28fa0f5a18b71646ed525ee9b93da08
karat <karateev.92@gmail.com>
2025-06-08 10:40:22 +0400

    Реализовано ограничение прав: добавлен владелец продукта, права на снятие публикации, ограничение редактирования и удаления

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,15 @@
 from django.views.generic import ListView, DetailView, TemplateView
-from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.generic import View
+from django.shortcuts import redirect, get_object_or_404
+
+
 from .models import Product
 from .forms import ProductForm
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import UpdateView
-from django.urls import reverse_lazy
-from django.views.generic.edit import DeleteView, CreateView
+
 
 class ProductListView(ListView):
     model = Product
@@ -23,22 +27,31 @@ class ProductDetailView(LoginRequiredMixin, DetailView):
 class ContactsView(TemplateView):
     template_name = 'catalog/contacts.html'
 
-class ProductUpdateView(LoginRequiredMixin, UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_update.html'
     context_object_name = 'product'
     login_url = 'users:login'
 
+    def test_func(self):
+        product = self.get_object()
+        return product.owner == self.request.user
+
     def get_success_url(self):
         return reverse_lazy('catalog:product_detail', kwargs={'pk': self.object.pk})
 
-class ProductDeleteView(LoginRequiredMixin, DeleteView):
+class ProductDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Product
     template_name = 'catalog/product_confirm_delete.html'
     context_object_name = 'product'
     success_url = reverse_lazy('catalog:home')
     login_url = 'users:login'
+
+    def test_func(self):
+        product = self.get_object()
+        user = self.request.user
+        return product.owner == user or user.has_perm('catalog.delete_product')
 
 class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
@@ -49,3 +62,12 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
     def get_success_url(self):
         return reverse_lazy('catalog:product_detail', kwargs={'pk': self.object.pk})
 
+class UnpublishProductView(LoginRequiredMixin, PermissionRequiredMixin, View):
+    permission_required = 'catalog.can_unpublish_product'
+    login_url = 'users:login'
+
+    def post(self, request, pk):
+        product = get_object_or_404(Product, pk=pk)
+        product.is_published = False
+        product.save()
+        return redirect('catalog:product_detail', pk=pk)
