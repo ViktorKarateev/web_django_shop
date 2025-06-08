fa8e464133679157f5f4b5d65570ac1473f63b99
karat <karateev.92@gmail.com>
2025-06-08 19:15:42 +0400

    Регистрирует модель CustomUser в админке

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_superuser')
+    list_filter = ('is_staff', 'is_superuser', 'groups')
+    ordering = ('email',)
+    search_fields = ('email', 'phone')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Персональная информация', {'fields': ('avatar', 'phone', 'country')}),
+        ('Права доступа', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Даты', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+    filter_horizontal = ('groups', 'user_permissions')
